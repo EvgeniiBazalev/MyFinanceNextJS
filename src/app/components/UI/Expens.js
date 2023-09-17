2bcfe6486bde0b36669ad8423314bee296cff5cc
EvgeniiBazalev <bazalevj@gmail.com>
2023-09-17 19:36:33 +0500

    Курсы валют в шапку

--- src/app/components/UI/Expens.js
+++ src/app/components/UI/Expens.js
@@ -5,6 +5,7 @@ import { useContext, useState, useEffect } from 'react';
 import AddExpens from './AddExpens';
 import TableOfExpens from './TableOfExpens';
 import Theme from '../Theme/Theme';
+import ExchangeRates from './dataFromAPI/ExchangeRates';
 import { ThemeContext } from '../../context/ThemeProvider';
 import { useSelector, useDispatch } from 'react-redux'
 import { addExpens, newExpens } from '@/app/store/expensSlice';
@@ -106,6 +107,8 @@ export default function Expens() {
                     <h1 className={styles.headOfFormText}>Мои финансы</h1>
                     <button className={`${styles.themeButton} + ${isDarkMode ? styles.darkMode : styles.lightMode}`} onClick={onThemeHandler}>{themeName}</button>
                 </div>
+
+                <ExchangeRates></ExchangeRates>
 
 
 
