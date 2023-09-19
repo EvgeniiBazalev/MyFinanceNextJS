55ea627d752c562651d55a492d0e3e7a4e30e9f6
EvgeniiBazalev <bazalevj@gmail.com>
2023-09-19 14:57:53 +0500

    Все курсы валют работают

--- src/app/components/ServerComponents/ExchangeRatesFromAPI.js
+++ src/app/components/ServerComponents/ExchangeRatesFromAPI.js
@@ -9,18 +9,44 @@ async function getData() {
         ETH: 0,
     }
 
-    const res = await fetch('https://api.coingate.com/v2/rates/merchant/EUR/RUB')
-    const EUR = await res.json();
-    if (!res.ok) {
-        throw new Error('Ошибка в получении информации о курсах валют')
+    const EURres = await fetch('https://api.coingate.com/v2/rates/merchant/EUR/RUB')
+    const EUR = await EURres.json();
+    if (!EURres.ok) {
+        throw new Error('Ошибка в получении информации о курсах валют EUR')
     }
+
+    const USDres = await fetch('https://api.coingate.com/v2/rates/merchant/USD/RUB')
+    const USD = await USDres.json();
+    if (!USDres.ok) {
+        throw new Error('Ошибка в получении информации о курсах валют USD')
+    }
+
+    const BTCres = await fetch('https://api.coingate.com/v2/rates/merchant/BTC/RUB')
+    const BTC = await BTCres.json();
+    if (!BTCres.ok) {
+        throw new Error('Ошибка в получении информации о курсах валют BTC')
+    }
+
+    const LTCres = await fetch('https://api.coingate.com/v2/rates/merchant/LTC/RUB')
+    const LTC = await LTCres.json();
+    if (!LTCres.ok) {
+        throw new Error('Ошибка в получении информации о курсах валют LTC')
+    }
+
+    const ETHres = await fetch('https://api.coingate.com/v2/rates/merchant/ETH/RUB')
+    const ETH = await ETHres.json();
+    if (!ETHres.ok) {
+        throw new Error('Ошибка в получении информации о курсах валют ETH')
+    }
+
+
 
     ExchangeRates = {
         EUR: EUR,
-        USD: 0,
-        BTC: 0,
-        LTC: 0,
-        ETH: 0,
+        USD: USD,
+        BTC: BTC,
+        LTC: LTC,
+        ETH: ETH,
     }
 
     return ExchangeRates
@@ -29,7 +55,6 @@ async function getData() {
 export default async function ExchangeRatesFromAPI() {
 
     const currencyRates = await getData()
-    console.log(currencyRates);
 
     return (
 
